eb83c98d4a62cbb007a8af9d3379424bfddaa0a0
LigiaGherasim <Ligia.Gherasim@theaccessgroup.com>
2020-10-27 22:59:22 +0200

    Laborator 5

---
+++ Gherasim-Ligia/L04/L04/Question.Domain/CreateQuestionWorkflow/InvalidQuestionException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Question.Domain.CreateQuestionWorkflow
+{
+    [Serializable]
+
+    public class InvalidQuestionException : Exception
+    {
+       
+            public InvalidQuestionException()
+            {
+            }
+
+            public InvalidQuestionException(string question) : base($"\n{question}\n is not valid. The value must be between 30 chars and 1000 chars!")
+            {
+            }
+            public InvalidQuestionException(string[] tags) : base("Invalid Tags!")
+            {
+            }
+            public InvalidQuestionException(string question, string[] tags) : base($"Question and Tags have invalid values.")
+            {
+            }
+    }
+}
