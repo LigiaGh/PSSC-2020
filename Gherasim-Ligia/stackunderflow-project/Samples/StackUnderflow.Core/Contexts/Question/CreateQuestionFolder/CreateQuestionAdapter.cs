f8ca445b1b076af42084f92fb9c53391b1f10e13
ligiagherasim <ligiagherasim@yahoo.com>
2020-12-01 16:22:18 +0200

    schemas for cmd and result files .. work to do

---
+++ Gherasim-Ligia/stackunderflow-project/Samples/StackUnderflow.Core/Contexts/Question/CreateQuestionFolder/CreateQuestionAdapter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackUnderflow.Domain.Core.Contexts.Question.CreateQuestionFolder
+{
+    public class CreateQuestionAdapter
+    {
+    }
+}
---
+++ Gherasim-Ligia/stackunderflow-project/Samples/StackUnderflow.Core/Contexts/Question/CreateQuestionFolder/CreateQuestionCmd.cs
@@ -0,0 +1,37 @@
+﻿using EarlyPay.Primitives.ValidationAttributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StackUnderflow.Domain.Core.Contexts.Question.CreateQuestionFolder
+{
+    public struct CreateQuestionCmd
+    {
+        public CreateQuestionCmd(Guid questionId, Guid userId, string userEmail, string title, string body, string tags)
+        {
+            QuestionId = questionId;
+            UserId = userId;
+            UserEmail = userEmail;
+            Title = title;
+            Body = body;
+            Tags = tags;
+        }
+
+        [GuidNotEmpty]
+        public Guid QuestionId { get; set; }
+        [GuidNotEmpty]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string UserEmail { get; set; }
+        [Required]
+        [MinLength(10),MaxLength(300)]
+        public string Title { get; set; }
+        [Required]
+        [MinLength(30), MaxLength(3000)]
+        public string Body { get; set; }
+        [Required]
+        public string Tags { get; set; }
+    }
+}
