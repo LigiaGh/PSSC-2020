227c992068b86dfd37accfaeb95a6acd2d54ba2a
ligiagherasim <ligiagherasim@yahoo.com>
2020-12-29 19:52:59 +0200

    work in progress part 1

---
+++ Gherasim-Ligia/StackunderflowFinalProject/start-project/Samples/StackUnderflow.Core/Contexts/Question/QuestionDomain.cs
@@ -0,0 +1,12 @@
+﻿using Access.Primitives.IO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackUnderflow.Domain.Core.Contexts.Question
+{
+    public static class QuestionDomain
+    {
+        public static Port<ICreateQuestion> CreateQuestion(CreateQuestionCmd command) => NewPort<CreateQuestionCmd, ICreateQuestionResult>(command);
+    }
+}
